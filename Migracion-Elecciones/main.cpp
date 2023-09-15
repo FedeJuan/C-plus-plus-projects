397f320b65576cb49c63235b7df8352ca346e886
Federico Juan <99974552+FedeJuan@users.noreply.github.com>
2023-09-15 13:30:48 -0300

    Update main.cpp

--- Migracion-Elecciones/main.cpp
+++ Migracion-Elecciones/main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void migracionVotos(const vector<int>& escrutinio, const vector<vector<int>>& migraciones, vector<int>& elecciones) {
+void migracionVotos(const vector<float>& escrutinio, const vector<vector<float>>& migraciones, vector<float>& elecciones) {
     int n = escrutinio.size();
 
     // Inicializar el vector de elecciones con ceros
@@ -24,18 +24,12 @@ void migracionVotos(const vector<int>& escrutinio, const vector<vector<int>>& mi
 int main() {
     vector<string> candidatos = {
             "Javier Milei", "Patricia Bullrich", "","Sergio Massa", "", "Juan Schiaretti",
-            "Miriam Bregman", "No lo tiene decidido", "Voto en blanco", "No va a votar"
-    };
-    vector<int> escrutinio = {7116352, 4022466 , 2675563, 5070104, 1390585, 907437, 442085, 911875, 1148342, 10474831};
-    vector<vector<int>> migraciones = {{95, 3, 0, 0, 0, 0, 0, 2, 0, 0}, {14, 82, 0, 0, 0, 0, 0, 3, 1, 0}, {9, 63, 0, 10, 0, 1, 1, 16, 0, 0}, {2, 0, 0, 92, 0, 0, 0, 0, 5, 0}, {1, 2, 0, 83, 0, 2, 1, 9, 0, 2}, {2, 15, 0, 21, 0, 31, 0, 32, 0, 0}, {0, 6, 0, 31, 0, 0, 46, 14, 4, 0}, {10, 6, 0, 23, 0, 6, 13, 37, 3, 1}, {5, 7, 0, 20, 0, 0, 0, 47, 18, 3}, {25, 9, 0, 24, 0, 1, 1, 11, 2, 28}};
-    vector<int> elecciones;
-
-    int sumatotal = 0;
-    for (int j = 0; j < escrutinio.size(); j++) {
-        sumatotal = escrutinio[j] + sumatotal;
-    }
+            "Miriam Bregman", "No tiene decidido","Voto en blanco", "No va a votar"};
+    vector<float> escrutinio = {7116352, 4022466 , 2675563, 5070104, 1390585, 907437, 442085, 911875, 1148342, 10474831};
+    vector<vector<float>> migraciones = {{95, 3, 0, 0, 0, 0, 0, 2, 0, 0}, {14, 82, 0, 0, 0, 0, 0, 3, 1, 0}, {9, 63, 0, 10, 0, 1, 1, 16, 0, 0}, {2, 0, 0, 92, 0, 0, 0, 0, 5, 0}, {1, 2, 0, 83, 0, 2, 1, 9, 0, 2}, {2, 15, 0, 21, 0, 31, 0, 32, 0, 0}, {0, 6, 0, 31, 0, 0, 46, 14, 4, 0}, {10, 6, 0, 23, 0, 6, 13, 37, 3, 1}, {5, 7, 0, 20, 0, 0, 0, 47, 18, 3}, {25, 9, 0, 24, 0, 1, 1, 11, 2, 28}};
+    vector<float> elecciones;
     migracionVotos(escrutinio, migraciones, elecciones);
-
+    int sumavalidos = elecciones[0] + elecciones[1] + elecciones[3] + elecciones[5] + elecciones[6] + elecciones[8];
     int i = 0;
     cout << "" << endl;
     cout << "Votos de las próximas elecciones generales:" << endl;
@@ -47,11 +41,19 @@ int main() {
     cout << "" << endl;
     i = 0;
     cout << "Resultados de las próximas elecciones generales:" << endl;
-    while (i < elecciones.size()) {
-        if (elecciones[i] != 0) {cout << candidatos[i] << ": " << (elecciones[i]*100)/sumatotal << " %" << endl;
+    while (i < (elecciones.size() - 3)) {
+        if (elecciones[i] != 0) {cout << candidatos[i] << ": " << (elecciones[i]*100)/sumavalidos << " %" << endl;
         }
         i++;
     }
+    sumavalidos = sumavalidos + elecciones[i];
+    cout << candidatos[i] << ": " << (elecciones[i]*100)/sumavalidos << " %" << endl;
+    i++;
+    cout << candidatos[i] << ": " << (elecciones[i]*100)/sumavalidos << " %" << endl;
+    i++;
+    sumavalidos = sumavalidos + elecciones[i];
+    cout << candidatos[i] << ": " << (elecciones[i]*100)/sumavalidos << " %" << endl;
+
 
     return 0;
 }
